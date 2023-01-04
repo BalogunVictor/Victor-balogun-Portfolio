92403074dfac307848b1bced9a61272a7f8547e5
balogunvictor <praisetheultimate@gmail.com>
2023-01-04 20:40:02 +0100

    created a Stateto implement menu Icon toggle function

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,10 +1,25 @@
 import './Header.scss';
-import { Link } from "react-router-dom";
+import { Link} from "react-router-dom";
+import { useState } from 'react';
+import {RiMenu4Line} from 'react-icons/ri';
+import {GrClose} from 'react-icons/gr'
 
 const Header = () => {
+  
+  const [clicked, setClicked] = useState(false);
+
+  const handleClicked = () => {
+    setClicked((prev) => !prev);
+  };
+
   return (
     <div className="header">
       <div className="headerWrapper">
+        <div className="menuIcon" onClick={handleClicked}>
+          <div className="menuIconWrapper">
+          {clicked ? <GrClose /> : <RiMenu4Line />}
+          </div>
+        </div>
         <Link  to="/" className="link">Home</Link>
         <Link to="/projects" className="link">Projects</Link>
         <Link to="/aboutme" className="link">About Me</Link>
